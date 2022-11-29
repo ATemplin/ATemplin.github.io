e35fcd4defdf0e6e1b028246d3ac3909c5b2f6bc
ATemplin <cadetatemplin@nomma.net>
2022-11-29 18:20:19 +0000

    Start of Step 3

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -42,7 +42,6 @@
     createCollectable(type.max, 200, 170, 6, 0.7);
     createCollectable(type.grace, 470, 250);
     createCollectable(type.kennedi, 825, 200);
-    createCollectable(type.db, 500, 90);
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
   }
